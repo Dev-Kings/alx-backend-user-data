0242e516ba2647be8d00f18d353b5b18d1aaac53
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-15 16:21:54 +0300

    Explored decoding using base64 module

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 BasicAuth module for managing basic authentication
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -29,3 +30,24 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes the Base64 string of the Authorization header
+
+        Args:
+            base64_authorization_header (str): The Base64 string
+
+        Returns:
+            str: The decoded value as UTF8 string, or None if invalid
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
