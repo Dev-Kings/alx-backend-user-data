9bbc81119f4a1fc15dc514cd31fdd10a4627c6f2
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-16 15:41:11 +0300

    updated before_request method to abort 401 if auth.authorization_header(request) and auth.session_cookie(request) return None

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,14 +32,15 @@ def before_request():
     """
     # Paths that do not require authentication
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/'],
 
     # Check if request path requires authentication
     if not auth.require_auth(request.path, excluded_paths):
         return
 
     # Check for Authorization header
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
         abort(401)  # Unauthorized
 
     request.current_user = auth.current_user(request)
