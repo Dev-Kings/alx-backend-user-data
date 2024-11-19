5f0967470b3635ce10482c94877aeeb8526ad037
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-19 12:01:09 +0300

    fixed pycodestyle

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,7 +12,7 @@ from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})   
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv("AUTH_TYPE")
 
@@ -31,6 +31,7 @@ elif getenv("AUTH_TYPE") == "session_db_auth":
     auth = SessionDBAuth()
 
 app.auth = auth
+
 
 @app.before_request
 def before_request():
