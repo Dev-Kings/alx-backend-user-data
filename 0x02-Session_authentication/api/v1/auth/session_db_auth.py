36833abef66b72e4de225cfe4f116f373c68bcd8
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-19 12:58:48 +0300

    added pytz to ensure all timezones are in utc

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -5,6 +5,7 @@ SessionDBAuth module
 from datetime import datetime, timedelta
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
+import pytz
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -44,17 +45,32 @@ class SessionDBAuth(SessionExpAuth):
             return None
 
         # Check session duration
+        print(f"Session duration set to: {self.session_duration} seconds")
         if self.session_duration <= 0:
             return user_session.user_id
 
-        # Verify session expiration
-        expiration_time = user_session.created_at + timedelta(
+        # Convert session created_at and current time to UTC for comparison
+        session_created_at_utc = user_session.created_at.replace(
+            tzinfo=pytz.utc)
+        current_time_utc = datetime.now(pytz.utc)
+
+        # Calculate expiration time
+        expiration_time = session_created_at_utc + timedelta(
             seconds=self.session_duration)
-        if expiration_time < datetime.now():
+
+        # Verify session expiration
+        # print(f"User session created at: {session_created_at_utc}")
+        # expiration_time = user_session.created_at + timedelta(
+        #     seconds=self.session_duration)
+        # print(f"Session expiration time: {expiration_time}")
+        # print(f"Current time: {current_time_utc}")
+
+        # if expiration_time < datetime.now():
+        if expiration_time < current_time_utc:
             # print("Session expired")
             return None
 
-        print(f"Session valid for user_id: {user_session.user_id}")
+        # print(f"Session valid for user_id: {user_session.user_id}")
         return user_session.user_id
 
     def destroy_session(self, request=None):
