ede1fb465be725d4038e39fa94a91dc28335a65e
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-21 19:37:53 +0300

    added flask's entrypoint

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This is a simple web application that uses the Flask framework to serve a
+"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify({'message': 'Bienvenue'})
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0" port="5000")
