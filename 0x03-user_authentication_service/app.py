44d6c8aeea4556dd62c9a0327c7195a2ff735380
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-21 19:50:42 +0300

    implemented route to handle registering on non-existent users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,9 +4,12 @@ This is a simple web application that uses the Flask framework
 to create a RESTful API.
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -15,6 +18,24 @@ def hello() -> str:
     This function is the starting point of the application.
     """
     return jsonify({'message': 'Bienvenue'})
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """
+    This function registers a new user.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        return jsonify({'message': 'Missing email or password'}), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({'email': user.email, 'message': 'user created'}), 201
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
 
 
 if __name__ == "__main__":
