2a75c487c49bdc2e7ebcc9b1aff462858ba983f4
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-21 22:03:19 +0300

    ref2

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,7 +61,7 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """
     Generates and returns a new UUID as a string.
     """
