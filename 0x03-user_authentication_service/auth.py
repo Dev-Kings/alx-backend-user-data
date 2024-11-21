0cbe7c55f1160426d5a51fe33c67aac2fa912e5a
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-21 19:27:33 +0300

    implemented method to register user if they do not exist

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,34 @@
 """
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user.
+        Args:
+            email (str): the user's email
+            password (str): the user's password
+        Returns:
+            User: the new User object
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
 
 
 def _hash_password(password: str) -> bytes:
